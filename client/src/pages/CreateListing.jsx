fcc1acfd1cadb1bdaf11fc2b065478a96d3250b8
Dipanshu ghime <dipanshughime@gmail.com>
2023-10-16 08:27:29 +0530

    modified

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -282,7 +282,7 @@ export default function CreateListing() {
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
                 {formData.type === 'rent' && (
-                  <span className='text-xs'>($ / month)</span>
+                  <span className='text-xs'>(Rs / month)</span>
                 )}
               </div>
             </div>
@@ -302,7 +302,7 @@ export default function CreateListing() {
                   <p>Discounted price</p>
 
                   {formData.type === 'rent' && (
-                    <span className='text-xs'>($ / month)</span>
+                    <span className='text-xs'>(Rs/ month)</span>
                   )}
                 </div>
               </div>
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -90,7 +90,7 @@ export default function Listing() {
           )}
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
-              {listing.name} - ${' '}
+              {listing.name} - Rs{' '}
               {listing.offer
                 ? listing.discountPrice.toLocaleString('en-US')
                 : listing.regularPrice.toLocaleString('en-US')}
